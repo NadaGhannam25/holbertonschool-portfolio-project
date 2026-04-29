8685149e176e5c08bc50eec78cbd75ac516a3cc1
SondosAlrubaish <sondos.alrubaih04@gmail.com>
2026-04-29 22:28:09 +0300

    Add database connection configuration with pg

---
+++ Stage 4/backend/config/db.js
@@ -0,0 +1,20 @@
+const { Pool } = require('pg')
+require('dotenv').config()
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+})
+
+pool.connect((err, client, release) => {
+  if (err) {
+    console.error('failed to connect with DB', err.message)
+  } else {
+    console.log('success to connect with DB')
+    release()
+  }
+})
+
+module.exports = pool
