035e728acbfd60faedb4e2721d2fcc456911bd13
SondosAlrubaish <sondos.alrubaih04@gmail.com>
2026-05-13 17:33:24 +0300

    Add files via upload

---
+++ Stage 4/backend-new/src/app.module.ts
@@ -0,0 +1,17 @@
+import { MiddlewareConsumer, Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { CategoriesModule } from './categories/categories.module';
+import { AuthModule } from './auth/auth.module';
+import { LoggerMiddleware } from './common/logger.middleware';
+
+@Module({
+  imports: [CategoriesModule, AuthModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
