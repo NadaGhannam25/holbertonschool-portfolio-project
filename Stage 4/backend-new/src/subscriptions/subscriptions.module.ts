12bfe4b6aa1ef938e5ebc690b7cbfff7eeef717f
SondosAlrubaish <sondos.alrubaih04@gmail.com>
2026-05-15 01:34:12 +0300

    Remove duplicate import of PdfController

--- Stage 4/backend-new/src/subscriptions/subscriptions.module.ts
+++ Stage 4/backend-new/src/subscriptions/subscriptions.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
+import { PdfController } from './pdf.controller';
 import { SubscriptionsController } from './subscriptions.controller';
 import { SubscriptionsService } from './subscriptions.service';
-import { PdfController } from './pdf.controller';
 
 @Module({
   controllers: [SubscriptionsController, PdfController],
   providers: [SubscriptionsService],
-  exports: [SubscriptionsService],
 })
 export class SubscriptionsModule {}
