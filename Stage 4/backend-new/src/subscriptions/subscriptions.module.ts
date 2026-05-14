915cef15e4212853749180fcd12638642cb6acc4
SondosAlrubaish <sondos.alrubaih04@gmail.com>
2026-05-15 02:00:43 +0300

    Remove PdfController from SubscriptionsModule

--- Stage 4/backend-new/src/subscriptions/subscriptions.module.ts
+++ Stage 4/backend-new/src/subscriptions/subscriptions.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { PdfController } from './pdf.controller';
 import { SubscriptionsController } from './subscriptions.controller';
 import { SubscriptionsService } from './subscriptions.service';
 
 @Module({
-  controllers: [SubscriptionsController, PdfController],
+  controllers: [SubscriptionsController],
   providers: [SubscriptionsService],
 })
 export class SubscriptionsModule {}
