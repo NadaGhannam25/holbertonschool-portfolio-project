7ebe41bb85c7b75a69083fcf3fcdebcbf5fe2d47
SondosAlrubaish <sondos.alrubaih04@gmail.com>
2026-05-15 01:43:53 +0300

    Add AnalyticsModule to AppModule imports

--- Stage 4/backend-new/src/app.module.ts
+++ Stage 4/backend-new/src/app.module.ts
@@ -3,11 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CategoriesModule } from './categories/categories.module';
 import { AuthModule } from './auth/auth.module';
-import { SubscriptionsModule } from './subscriptions/subscriptions.module';
 import { LoggerMiddleware } from './common/logger.middleware';
+import { SubscriptionsModule } from './subscriptions/subscriptions.module';
+import { AnalyticsModule } from './analytics/analytics.module';
 
 @Module({
-  imports: [CategoriesModule, AuthModule, SubscriptionsModule],
+  imports: [CategoriesModule, AuthModule, SubscriptionsModule, AnalyticsModule],
   controllers: [AppController],
   providers: [AppService],
 })
