5a483a889ab0c9e064d790c010b047eb949c6a9f
Shaden Alalwani <viuxwl7@gmail.com>
2026-05-19 13:04:56 +0300

    Add PdfController to SubscriptionsModule

--- Stage 4/backend-new/src/subscriptions/subscriptions.module.ts
+++ Stage 4/backend-new/src/subscriptions/subscriptions.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+
 import { SubscriptionsController } from './subscriptions.controller';
+import { PdfController } from './pdf.controller';
+
 import { SubscriptionsService } from './subscriptions.service';
 
 @Module({
-  controllers: [SubscriptionsController],
+  controllers: [
+    SubscriptionsController,
+    PdfController,
+  ],
   providers: [SubscriptionsService],
 })
 export class SubscriptionsModule {}
