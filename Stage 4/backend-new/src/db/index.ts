c22fcee921ed557ffb33ae49cfcead911a1453a8
Shaden Alalwani <viuxwl7@gmail.com>
2026-05-24 00:42:47 +0300

    Update index.ts

--- Stage 4/backend-new/src/db/index.ts
+++ Stage 4/backend-new/src/db/index.ts
@@ -2,7 +2,3 @@ import "dotenv/config";
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
 import * as schema from "./schema";
-
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
