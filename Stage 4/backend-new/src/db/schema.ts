beb4588a7ec434b50fa296149fe3d81d3fdd60de
Shaden Alalwani <viuxwl7@gmail.com>
2026-05-24 00:47:47 +0300

    Update schema.ts

--- Stage 4/backend-new/src/db/schema.ts
+++ Stage 4/backend-new/src/db/schema.ts
@@ -13,7 +13,7 @@ export const users = pgTable('users', {
   id: serial('id').primaryKey(),
   name: text('name').notNull(),
   email: text('email').notNull().unique(),
-  passwordHash: text('password_hash').notNull(),
+  passwordHash: tpassword_hash').notNull(),
   resetPasswordToken: text('reset_password_token'),
   resetPasswordExpires: timestamp('reset_password_expires'),
   createdAt: timestamp('created_at').defaultNow(),
