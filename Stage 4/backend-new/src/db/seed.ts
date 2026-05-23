925b3ba92f13b62df346aad813721a9b4da5401e
Shaden Alalwani <viuxwl7@gmail.com>
2026-05-24 00:56:31 +0300

    Update seed.ts

--- Stage 4/backend-new/src/db/seed.ts
+++ Stage 4/backend-new/src/db/seed.ts
@@ -2,8 +2,3 @@ import "dotenv/config";
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
 import { categories } from "./schema";
-
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
-});
