1de1bc4efa807dab676e923d7c571127a99fba49
Shaden Alalwani <viuxwl7@gmail.com>
2026-05-24 00:46:09 +0300

    Update schema.ts

--- Stage 4/backend-new/src/db/schema.ts
+++ Stage 4/backend-new/src/db/schema.ts
@@ -4,14 +4,3 @@ import {
   text,
   timestamp,
   integer,
-  decimal,
-  date,
-  boolean,
-} from 'drizzle-orm/pg-core';
-
-export const users = pgTable('users', {
-  id: serial('id').primaryKey(),
-  name: text('name').notNull(),
-  email: text('email').notNull().unique(),
-  passwordHash: text('password_hash').notNull(),
-  resetPasswordToken: text('reset_password_toke
