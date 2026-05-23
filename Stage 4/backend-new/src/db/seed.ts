540c3a0347aaee8d20274721ecd83e07bbe5d745
Shaden Alalwani <viuxwl7@gmail.com>
2026-05-24 00:58:03 +0300

    Update seed.ts

--- Stage 4/backend-new/src/db/seed.ts
+++ Stage 4/backend-new/src/db/seed.ts
@@ -3,11 +3,7 @@ import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
-});
-
-const db = drizzle(pool);
+  connectionString: process.en;
 
 const defaultCategories = [
   "ترفيه",
