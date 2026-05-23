f0ed9b0bee5d9924f6c817f535b3d43425455abb
Nada Al-Mutairi <nada.ghannam19@gmail.com>
2026-05-23 17:06:26 +0300

    Add TopLogo component to display logo

---
+++ Stage 4/Front/dierha-frontend/src/components/TopLogo.tsx
@@ -0,0 +1,11 @@
+import logo from "../assets/dierha-logo.png";
+
+function TopLogo() {
+    return (
+        <div className="top-logo">
+            <img src={logo} alt="Dierha Logo" />
+        </div>
+    );
+}
+
+export default TopLogo;
